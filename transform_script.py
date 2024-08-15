33f5d28560f34bca3d76e636bc79c70131c4b0e3
YogeshMourya310 <112762036+YogeshMourya310@users.noreply.github.com>
2024-08-15 17:25:09 +0530

    Update transform_script.py

--- transform_script.py
+++ transform_script.py
@@ -36,8 +36,8 @@ ChangeSchema_node1723638356856 = ApplyMapping.apply(
         ("store_location", "string", "store_location", "string"),
         ("week_of_year", "string", "week_of_year", "int"),  # Changed to int
         ("purchase_frequency", "string", "purchase_frequency", "string"),
-        ("online_purchases", "string", "online_purchases", "string"),
-        ("in_store_purchases", "string", "in_store_purchases", "timestamp"),
+        ("online_purchases", "string", "online_purchases", "int"),
+        ("in_store_purchases", "string", "in_store_purchases", "int"),
         ("total_sales", "string", "total_sales", "double"),  # Changed to double
         ("product_rating", "string", "product_rating", "float"),
         ("season", "string", "season", "string")
