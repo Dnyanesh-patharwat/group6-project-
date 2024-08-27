71ad11e43187d162fc9e77533f4325052c1f8657
YogeshMourya310 <112762036+YogeshMourya310@users.noreply.github.com>
2024-08-27 19:27:05 +0530

    Update transform_script.py

--- transform_script.py
+++ transform_script.py
@@ -1,10 +1,8 @@
 import sys
-from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from awsglue.dynamicframe import DynamicFrame
 from pyspark.sql import functions as F
 
 # Initialize the job
@@ -17,26 +15,82 @@ job.init(args['JOB_NAME'], args)
 
 # Read CSV data from S3 with headers
 s3_csv_path = "s3://group6-datalake/output/combine_data.csv"
+
 df = spark.read.option("header", "true").csv(s3_csv_path)
 
 # Schema transformation: Convert columns to appropriate data types
-df = df.withColumn("customer_id", F.col("customer_id").cast("int"))
-df = df.withColumn("age", F.col("age").cast("int"))
-df = df.withColumn("quantity", F.col("quantity").cast("int"))
-df = df.withColumn("unit_price", F.col("unit_price").cast("double"))
-df = df.withColumn("week_of_year", F.col("week_of_year").cast("int"))
-df = df.withColumn("online_purchases", F.col("online_purchases").cast("int"))
-df = df.withColumn("in_store_purchases", F.col("in_store_purchases").cast("int"))
-df = df.withColumn("total_sales", F.col("total_sales").cast("double"))
-df = df.withColumn("product_rating", F.col("product_rating").cast("float"))
+df = df.withColumn("Order ID", F.col("Order ID").cast("string"))
 
-# Example transformation: Convert `transaction_date` to a specific date format
-df = df.withColumn("transaction_date", F.to_date(F.col("transaction_date"), "dd-MM-yyyy"))
+df = df.withColumn("Ship Mode", F.col("Ship Mode").cast("string"))
+
+df = df.withColumn("Customer ID", F.col("Customer ID").cast("string"))
+
+df = df.withColumn("Customer Name", F.col("Customer Name").cast("string"))
+
+df = df.withColumn("Segment", F.col("Segment").cast("string"))
+
+df = df.withColumn("City", F.col("City").cast("string"))
+
+df = df.withColumn("State", F.col("State").cast("string"))
+
+df = df.withColumn("Country", F.col("Country").cast("string"))
+
+df = df.withColumn("Market", F.col("Market").cast("string"))
+
+df = df.withColumn("Region", F.col("Region").cast("string"))
+
+df = df.withColumn("Product ID", F.col("Product ID").cast("string"))
+
+df = df.withColumn("Category", F.col("Category").cast("string"))
+
+df = df.withColumn("Product Name", F.col("Product Name").cast("string"))
+
+df = df.withColumn("Sales", F.round(F.col("Sales").cast("float"), 2))
+
+df = df.withColumn("Quantity", F.col("Quantity").cast("int"))
+
+df = df.withColumn("Profit", F.col("Profit").cast("double"))
+
+df = df.withColumn("Shipping Cost", F.col("Shipping Cost").cast("float"))
+
+df = df.withColumn("Order Priority", F.col("Order Priority").cast("string"))
+
+df = df.withColumnRenamed("Order ID", "Order_ID")
+
+df = df.withColumnRenamed("Ship Mode", "Ship_Mode")
+
+df = df.withColumnRenamed("Customer ID", "Customer_ID")
+
+df = df.withColumnRenamed("Customer Name", "Customer_Name")
+
+df = df.withColumnRenamed("Product ID", "Product_ID")
+
+df = df.withColumnRenamed("Order Priority", "Order_Priority")
+
+df = df.withColumnRenamed("order_date", "Order_Date")
+
+# Example transformation: Convert `Order Date` to a specific date format
+df = df.withColumn("Order_Date", F.to_date(F.col("Order_Date"), "dd-MM-yyyy"))
+
+# Assuming you have a column named 'date' in your DataFrame
+df = df.withColumn("year", F.year(F.col("Order_Date")))
+
+df = df.withColumn("month_name", F.date_format(F.col("Order_Date"), "MMMM"))
+
+df = df.withColumn("week_day", F.date_format(F.col("Order_Date"), "EEEE"))
+
+df = df.withColumn("Cost_Price", F.col("Shipping Cost") + F.col("Sales"))
+
+cols_to_drop = ["Row ID", "Shipping Cost", "Ship Date","Postal Code"]
+
+df = df.drop(*cols_to_drop)
+
+df.printSchema()
 
 df = df.coalesce(1)
 
 # Write the data back to S3 in Parquet format without overwrite
 output_path = "s3://group6-datawarehouse/output/"
-df.write.parquet(output_path)
+df.write.parquet(output_path, mode="overwrite")
 
 job.commit()
